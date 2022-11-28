698cbdf563a9f4b856d8cea2b14f0f6e37dbeb5b
Henry Jackson <95002914+hejackson2@users.noreply.github.com>
2022-11-28 12:36:57 -0500

    fix date length issue and add output to README.md

--- cisco-ping.sh
+++ cisco-ping.sh
@@ -7,7 +7,7 @@ trap controlSee SIGINT
 while :; do
         d=$(date +"%Y%m%d-%H%M%S")
         echo -n "${d}: "
-        for x in $(seq $(($(tput cols)-$(date | wc -c)-3))); do
+        for x in $(seq $(($(tput cols)-$(echo ${d} | wc -c)-3))); do
                 if ping -q -A -c 1 -t 1 ${1} >/dev/null; then
                         echo -n "!"
                 else 
